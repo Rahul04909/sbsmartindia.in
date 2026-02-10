e35454cbb7d60b22cc180d1fcd0ab3e01a510384
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-10 16:39:22 +0530

    updated ccavenue

--- ccavRequestHandler.php
+++ ccavRequestHandler.php
@@ -74,8 +74,8 @@ if(isset($_POST['checkout_submit'])) {
         ?>
         <form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
             <?php
-            echo "<input type=hidden name=encRequest value=$encrypted_data>";
-            echo "<input type=hidden name=access_code value=$access_code>";
+            echo "<input type='hidden' name='encRequest' value='$encrypted_data'>";
+            echo "<input type='hidden' name='access_code' value='$access_code'>";
             ?>
         </form>
         <script language='javascript'>document.redirect.submit();</script>
