63e74d273e3161705ca165dd1571576fceeb8054
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-10 20:35:03 +0530

    updated

--- database/sql/setup_otp.php
+++ database/sql/setup_otp.php
@@ -1,8 +1,5 @@
 <?php
-include_once "../db_config.php";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+require_once '../db_config.php';
 
 // Check connection
 if ($conn->connect_error) {
